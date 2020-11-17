823497be296b4393c2c103559e8b658aeb38db1c
Necip1986 <adnan7klc@gmail.com>
2020-11-17 22:51:32 +0200

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -11,6 +11,7 @@ public class BirinciGun {
 		System.out.println("11:05 - local");
 		System.out.println("11:23 - github.com");
 		System.out.println("Ben buradan conflict beklemiyorum..");
+		System.out.println("Local'den yepyeni bir satir");
 
 
 
