a8297c7ee67155895b41a88111264b793444e09a
Necip1986 <adnan7klc@gmail.com>
2020-11-18 21:53:14 +0200

    feature ekleme

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -13,6 +13,7 @@ public class BirinciGun {
 		System.out.println("Ben buradan conflict beklemiyorum..");
 		System.out.println("Yepyeni bir satir");
 		System.out.println("10:26 - Feature Branch");
+		System.out.println("21:52 - Feature ekleme");
 
 
 
