c6d9076aa24ee2faee0cad13b447a9f24a59c7dc
Necip1986 <adnan7klc@gmail.com>
2020-11-18 23:02:16 +0200

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,7 +3,7 @@ public class BirinciGun {
 
 	public static void main(String[] args) {
 		
-		System.out.println("11:57 - Birinci Gun");
+		System.out.println("Ben 12:00'da bu satiri feature branch'ta degistirdim");
 		System.out.println("Ikinci commit");
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - Ikinci Gun");
@@ -17,6 +17,7 @@ public class BirinciGun {
 		System.out.println("22:06 - Master branch'a ekleme");
 		System.out.println("11:20 - Feature branch");
 		System.out.println("11:45 - feature branch");
+		
 
 
 
